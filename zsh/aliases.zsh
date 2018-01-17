4aee94d1066f3d2704a6ecb20f56dd999dae627c
jgartland <jgartland@MBP-1014771.local>
2018-01-17 10:46:30 -0500

    adding a few minor mods to yadr files for my system

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -36,7 +36,7 @@ if [[ $platform == 'linux' ]]; then
   alias ll='ls -alh --color=auto'
   alias ls='ls --color=auto'
 elif [[ $platform == 'darwin' ]]; then
-  alias ll='ls -alGh'
+  alias ll='ls -alGhFp'
   alias ls='ls -Gh'
 fi
 
